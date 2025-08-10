f444adbb2b72994d956ae447c6f4584e93866330
ff-server <joe@fastfamiliar.com>
2025-08-10 19:38:07 +0000

    updates

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 	var header = document.getElementById("mainHeader");
 
 	function changeHeader() {
-		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
-	}
+          header.classList.add("header-background");
+        }
+
 
 	var didScroll = false;
 
@@ -21,10 +21,10 @@
 
 	changeHeader();
 
-<!-- 	document.getElementById("open-nav").addEventListener("click", function (event) {
+	document.getElementById("open-nav").addEventListener("click", function (event) {
 		event.preventDefault();
 		document.body.classList.toggle("nav-open");
-		changeHeader(); -->
+		changeHeader();
 	});
 
 	$("a[href*=\\#]").on("click", function (event) {
